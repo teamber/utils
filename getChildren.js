9209d963cffc3afa1f57ec384b6aeb4a7cf6a257
teamber <122089175+teamber@users.noreply.github.com>
2023-05-02 14:50:17 +0200

    Update getChildren.js

--- getChildren.js
+++ getChildren.js
@@ -1,10 +1,10 @@
-    // Return how many children an article has
-    function getChildren(data, articleId) {
-        const element = data.find(article => article.tmp_ordre === articleId);
-        if (!element) {
-            return [];
-        }
-        const children = data.filter(article => article.tmp_parent === articleId);
-        const flattenedChildren = children.flatMap((child) => getChildren(data, child.tmp_ordre));
-        return [element, ...flattenedChildren];
+// Return how many children an article has
+export function getChildren(data, articleId) {
+    const element = data.find(article => article.tmp_ordre === articleId);
+    if (!element) {
+        return [];
     }
+    const children = data.filter(article => article.tmp_parent === articleId);
+    const flattenedChildren = children.flatMap((child) => getChildren(data, child.tmp_ordre));
+    return [element, ...flattenedChildren];
+}
